f76bfece88d1cd1b9738e33c6be564bc0783250f
Arthur Tse <17014719+arty-tse@users.noreply.github.com>
2020-07-12 14:55:05 -0700

    Updates

---
+++ precache-manifest.d0fcbf8dfea088258db17a2166654649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e154efe9da90134b4c4321a30f8692d7",
+    "url": "/ms-calcs/index.html"
+  },
+  {
+    "revision": "ae948b38eaf8063de090",
+    "url": "/ms-calcs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a0284198c15ff4fa419b",
+    "url": "/ms-calcs/static/css/main.84bedda0.chunk.css"
+  },
+  {
+    "revision": "ae948b38eaf8063de090",
+    "url": "/ms-calcs/static/js/2.b913dc3d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ms-calcs/static/js/2.b913dc3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0284198c15ff4fa419b",
+    "url": "/ms-calcs/static/js/main.fae7016b.chunk.js"
+  },
+  {
+    "revision": "54e31493822ee9637d23",
+    "url": "/ms-calcs/static/js/runtime-main.6610f0b6.js"
+  }
+]);
